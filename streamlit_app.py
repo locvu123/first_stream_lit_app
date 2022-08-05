6a4eeb66dc9623f6c2037e6817abda7bf2d3384e
Loc Vu <32008504+locvu123@users.noreply.github.com>
2022-08-05 22:17:25 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy diners')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Bluebery')
-streamlit.text('Spinach & Kale')
-streamlit.text('Hard boiled egg - free range')
+streamlit.text('🥣 Omega 3 and Bluebery')
+streamlit.text('🥗 Spinach & Kale')
+streamlit.text('🐔 Hard boiled egg - free range')
+streamlit.text('🥑🍞 Avocado & Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
