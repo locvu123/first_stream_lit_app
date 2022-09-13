28f2eb2e100f24e3dd76b619e0c832d33d498703
Loc Vu <32008504+locvu123@users.noreply.github.com>
2022-09-14 09:02:02 +1000

    Update streamlit_app.py
    added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.text('🥗 Spinach & Kale')
 streamlit.text('🐔 Hard boiled egg - free range')
 streamlit.text('🥑🍞 Avocado & Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
