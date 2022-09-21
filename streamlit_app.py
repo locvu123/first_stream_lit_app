812c7a5409ce23da78acaf9b09844d2f0f09d32c
Loc Vu <32008504+locvu123@users.noreply.github.com>
2022-09-21 17:21:40 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 # New section
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response, json())
+streamlit.text(fruityvice_response.json())
