653e5267494714cbd378b59efead3df054e0f59a
Loc Vu <32008504+locvu123@users.noreply.github.com>
2022-09-21 16:55:04 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,15 +12,4 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Strawberries', 'Avocado', 'Pineapple'])
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), [])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
