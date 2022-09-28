5335bad4cea2a7a676d51562d8de21178d816d4a
Loc Vu <32008504+locvu123@users.noreply.github.com>
2022-09-28 15:25:17 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.dataframe(fruits_to_show)
 # create function
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruityvice_normalized = panda.json_normalize(fruityvice_response.json())
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
 # new section to display api response
