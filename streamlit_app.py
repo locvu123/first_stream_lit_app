9b6f680ee9fe2b59797983dbfb72eefa2dc44a25
Loc Vu <32008504+locvu123@users.noreply.github.com>
2022-09-28 16:06:03 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ if streamlit.button("Get Fruit Load List"):
 # import snowflake.connector
 
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
+# my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # add_my_fruit = my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('papaya from streamit') ")
 # my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
@@ -83,7 +83,7 @@ if streamlit.button("Get Fruit Load List"):
 # my_data_rows = my_cur.fetchall()
   my_data_rows = get_fruit_load_list()
 # streamlit.text("Hello from Snowflake:")
-streamlit.text("The fruit load list contains followings:")
+streamlit.text("The fruit load list contains:")
 # streamlit.text(my_data_row)
 # streamlit.dataframe(my_data_row)
   streamlit.dataframe(my_data_rows)
