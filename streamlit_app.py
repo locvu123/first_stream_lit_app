ad32a810c0c5ff1b96c497ca3472643827d4d52d
Loc Vu <32008504+locvu123@users.noreply.github.com>
2022-09-28 11:56:35 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+add_my_fruit = my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('jackfruit') ")
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 # my_data_row = my_cur.fetchone()
 my_data_rows = my_cur.fetchall()
