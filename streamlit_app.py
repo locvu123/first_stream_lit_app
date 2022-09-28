b5beec9cbc402da991a3792123d316858954d652
Loc Vu <32008504+locvu123@users.noreply.github.com>
2022-09-28 14:22:32 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 # if not fruit_choice :
 streamlit.error("Please select a FRUIT to get information.")
- else:
+# else:
  # streamlit.write('The user entered ', fruit_choice)
 
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
