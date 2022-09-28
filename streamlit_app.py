d660ea5effb5d07f767340053dd9283c953bcd73
Loc Vu <32008504+locvu123@users.noreply.github.com>
2022-09-28 15:50:33 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,20 +61,27 @@ try:
 except URLError as e:
  streamlit.error()
 
+def get_fruit_load_list()
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_loadf_list")
+    return my_cur.fetchall()
+  
+ # add a button
+if streamlit.button('Get Fruit Load List"):
 
-
-streamlit.stop()
+# streamlit.stop()
 # import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # add_my_fruit = my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('papaya from streamit') ")
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 # my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
+# my_data_rows = my_cur.fetchall()
+  my_data_rows = get_fruit_load_list()
 # streamlit.text("Hello from Snowflake:")
 streamlit.text("The fruit load list contains followings:")
 # streamlit.text(my_data_row)
 # streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
+  streamlit.dataframe(my_data_rows)
